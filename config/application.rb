6a04bd22fffd0eafa182a2ca3c0675afed626780
ShahriarAzizAakash <aakashaayat@gmail.com>
2019-02-21 22:04:22 +0600

    added another origin from heroku to application.rb

--- config/application.rb
+++ config/application.rb
@@ -24,7 +24,7 @@ module AuthApp
 
     config.middleware.insert_before 0, Rack::Cors do
       allow do
-        origins 'http://localhost:3000'
+        origins 'http://localhost:3000', 'https://5x9l0v3vl4.codesandbox.io/'
         resource '*', :headers => :any, :methods => [:get, :post, :put, :delete, :options]
       end
     end
