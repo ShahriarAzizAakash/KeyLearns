2c6295580f9f8c7dcc2342dbd1d5a444bad177e2
ShahriarAzizAakash <aakashaayat@gmail.com>
2019-02-21 22:39:50 +0600

    changed headers in applicationcontroller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,7 @@
 class ApplicationController < ActionController::API
     include ActionController::HttpAuthentication::Token::ControllerMethods
-    
+    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
+    headers['Access-Control-Request-Method'] = '*'
+    headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
 end
--- config/application.rb
+++ config/application.rb
@@ -24,7 +24,7 @@ module AuthApp
 
     config.middleware.insert_before 0, Rack::Cors do
       allow do
-        origins 'x'
+        origins '*'
         resource '*', :headers => :any, :methods => [:get, :post, :put, :delete, :options]
       end
     end
