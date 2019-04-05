13f1dee8d0b3dff8f037b874329105293536886f
ShahriarAzizAakash <aakashaayat@gmail.com>
2019-04-05 21:03:52 +0600

    cookie csrf

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,16 +1,7 @@
 class ApplicationController < ActionController::API
     #include ActionController::HttpAuthentication::Token::ControllerMethods
-    after_action :set_csrf_cookie
-
-  
     include JWTSessions::RailsAuthorization
     rescue_from JWTSessions::Errors::Unauthorized, with: :not_authorized
-
-    protected
-
-        def set_csrf_cookie
-            cookies["X-CSRF-Token"] = form_authenticity_token
-        end
 
     private
 
