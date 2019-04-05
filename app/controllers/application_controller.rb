1924b9e32634b848ff10e2e63eeffc1e45b41115
ShahriarAzizAakash <aakashaayat@gmail.com>
2019-04-05 21:15:56 +0600

    indent

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,24 +1,16 @@
 class ApplicationController < ActionController::API
     #include ActionController::HttpAuthentication::Token::ControllerMethods
-    after_action :set_csrf_cookie
 
-  
     include JWTSessions::RailsAuthorization
     rescue_from JWTSessions::Errors::Unauthorized, with: :not_authorized
-
-    protected
-
-        def set_csrf_cookie
-            cookies["X-CSRF-Token"] = form_authenticity_token
-        end
 
     private
 
         def current_user
-        @current_user ||= User.find(payload['user_id'])
+            @current_user ||= User.find(payload['user_id'])
         end
 
         def not_authorized
-        render json: { error: 'Not authorized' }, status: :unauthorized
+            render json: { error: 'Not authorized' }, status: :unauthorized
         end
 end
