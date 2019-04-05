2022b45f933c7d02f382ddad8f23ea91ee7e0856
ShahriarAzizAakash <aakashaayat@gmail.com>
2019-04-05 22:04:57 +0600

    heroku

--- config/application.rb
+++ config/application.rb
@@ -25,7 +25,7 @@ module AuthApp
     config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins '*'
-        resource '*', :headers => :any, credentials: true, :methods => [:get, :post, :put, :delete, :options]
+        resource '*', :headers => :any, :methods => [:get, :post, :put, :delete, :options]
       end
     end
 
