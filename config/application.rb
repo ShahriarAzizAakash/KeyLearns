cd27ad4d777841c9e4d0ccc178cef06b091f6660
ShahriarAzizAakash <aakashaayat@gmail.com>
2019-04-17 11:36:57 +0600

    forcing ssl on production

--- config/application.rb
+++ config/application.rb
@@ -22,13 +22,6 @@ module AuthApp
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
-    config.middleware.insert_before 0, Rack::Cors do
-      allow do
-        origins 'http://localhost:3000', 'https://kkeylearns.herokuapp.com'
-        resource '*', :headers => :any, :methods => [:get, :post, :put, :delete, :options]
-      end
-    end
-
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
