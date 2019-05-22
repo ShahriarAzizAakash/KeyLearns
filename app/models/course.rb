b853c2e1673423f820ebc790c0e5f17800c38ace
ShahriarAzizAakash <aakashaayat@gmail.com>
2019-05-22 21:23:51 +0600

    validation of course attribute presence

--- app/models/course.rb
+++ app/models/course.rb
@@ -3,5 +3,5 @@ class Course < ApplicationRecord
     has_many :enrollments
     has_many :contents
     mount_uploader :featuredImage, FeaturedImageUploader
-    validates_presence_of :tags
+    validates_presence_of :tags, :title, :description, :requirements, :price, :featuredImage
 end
